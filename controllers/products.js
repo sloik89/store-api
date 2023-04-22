70316c5551a042175d9da347dd5349d60c7882dc
sloik89 <sloik89@gmail.com>
2023-04-22 19:43:20 +0200

    handle pagination

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -21,6 +21,15 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   }
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    console.log(fieldsList);
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, numberOfHits: products.length });
 };
