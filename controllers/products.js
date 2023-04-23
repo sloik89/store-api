0e65b9d8b9113cd876962d0313b86bf9cabb7b88
sloik89 <sloik89@gmail.com>
2023-04-23 16:31:19 +0200

    complete api

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,11 @@
 const Products = require("../models/products");
 const getAllProductsStatic = async (req, res) => {
-  const products = await Products.find({}).sort("price");
+  const products = await Products.find({ price: { $gt: 100 } });
   res.status(200).json({ products });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFillters } = req.query;
+  // build query object
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -15,6 +16,30 @@ const getAllProducts = async (req, res) => {
   if (name) {
     // use query operators in mongo db
     queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFillters) {
+    // object with mongo propery
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    console.log(numericFillters);
+    let fillters = numericFillters.replace(regEx, (match) => {
+      return `-${operatorMap[match]}-`;
+    });
+    console.log(fillters);
+    const options = ["price", "rating"];
+    fillters = fillters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+    console.log(queryObject);
   }
   let result = Products.find(queryObject);
   if (sort) {
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ### Store API
 
-- Fillter data using featured, company, name
+Simple Store API
 
 ## Implementing sort
 
@@ -13,3 +13,24 @@ if (sort) {
 const products = await result;
 res.status(200).json({ products, numberOfHits: products.length });
 ```
+
+### URLS
+
+- # Sort base by featured
+  - [domain]/api/products?featured=true
+- # Sort base by company name
+  - [domain]/api/products?company=ikea
+- # Sort base search name
+- [domain]/api/products?name=somename
+- # Sort from low price
+  - [domain]/api/products?sort=price
+- # Sort from high price
+  - [domain]/api/products?sort=-price
+- # Sort from name a - z
+  - [domain]/api/products?sort=name
+- # Sort from name z - a
+  - [domain]/api/products?sort=-name
+- # Only fields
+  - [domain]/api/products?fields=name,price,company
+- # Numeric Fillters
+  - [domain]/api/products?numericFillters=price>=100,rating=5
